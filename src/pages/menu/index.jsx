52f1e1e19684d0f051f67d8cd26a71b61e299897
realeve@qq.com <realeve@qq.com>
2018-09-25 01:26:44 +0800

    菜单全部展开与折叠

--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -1,23 +1,25 @@
 import React, { Component } from "react";
 import { connect } from "dva";
-import { Row, Col, Card, Tabs } from "antd";
+import { Row, Col, Card, Tabs, Button, Divider } from "antd";
 
 import SortableTree from "react-sortable-tree";
 import "react-sortable-tree/style.css";
 import FileExplorerTheme from "react-sortable-tree-theme-minimal";
 // import FileExplorerTheme from "react-sortable-tree-theme-full-node-drag";
-
+import * as treeUtil from "./tree-data-utils";
 import styles from "./index.less";
+
+const R = require("ramda");
 
 class VTree extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      expanded: true,
       treeData: [
         {
           title: "Chicken",
-          expanded: true,
           id: 23,
           children: [
             { title: "Egg", id: 24 },
@@ -47,11 +49,23 @@ class VTree extends Component {
     console.log(treeData);
   };
 
+  expandAll = () => {
+    let { expanded, treeData } = this.state;
+    treeData = treeUtil.toggleExpandedForAll({ expanded, treeData });
+    this.setState({ treeData, expanded: !expanded });
+  };
+
   render() {
     return (
       <Card title="菜单设置">
         <Row>
           <Col md={8} sm={24}>
+            <div className={styles.action}>
+              <Button onClick={this.expandAll}>
+                {this.state.expanded ? "全部展开" : "全部折叠"}
+              </Button>
+            </div>
+            <Divider dashed />
             <div style={{ height: 500 }} className={styles.container}>
               <SortableTree
                 treeData={this.state.treeData}
