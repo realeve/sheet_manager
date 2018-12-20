c8d1787a0f6838418f442b69a7b20a4ae6febc2f
realeve@qq.com <realeve@qq.com>
2018-12-20 17:25:02 +0800

    更新单元测试

--- src/pages/chart/utils/lib.test.js
+++ src/pages/chart/utils/lib.test.js
@@ -8,6 +8,100 @@ describe('图表lib', () => {
 
   test('字符串转数字', () => {
     expect(lib.str2Num('2018')).toBe(2018);
-    expect(lib.str2Num('2018.2')).toBe('2018.2');
+    expect(lib.str2Num('2018.2')).toBe(2018.2);
+  });
+
+  test('是否是日期', () => {
+    expect(lib.isDate('2018')).toBe(false);
+    expect(lib.isDate('2018-12-21')).toBe(true);
+    expect(lib.isDate('20181221')).toBe(true);
+    expect(lib.isDate('0018-12-21')).toBe(false);
+  });
+
+  test('是否需要转换', () => {
+    expect(lib.needConvertDate('2018-12-21')).toBe(true);
+    expect(lib.needConvertDate('20181221')).toBe(true);
+    expect(lib.needConvertDate('201812')).toBe(true);
+    expect(lib.needConvertDate('2018-12')).toBe(true);
+  });
+
+  test('按键值分组', () => {
+    expect(
+      lib.getDataByIdx({
+        key: 'a',
+        data: [{ a: 1 }, { a: 2 }, { a: 3 }, { a: 4 }, { a: 1 }]
+      })
+    ).toEqual([1, 2, 3, 4, 1]);
+  });
+
+  test('按键值获取唯一值', () => {
+    expect(
+      lib.getDataByIdx({
+        key: 'a',
+        data: [{ a: 1 }, { a: 2 }, { a: 4 }, { a: 4 }, { a: 1 }]
+      })
+    ).toEqual([1, 2, 4, 4, 1]);
+  });
+
+  test('按键值获取唯一值', () => {
+    expect(
+      lib.getDataByKeys({
+        keys: ['a', 'b'],
+        data: [
+          { a: 1, b: 2, c: 3, d: 4 },
+          { a: 2, b: 2, c: 3, d: 4 },
+          { a: 4, b: 2, c: 3, d: 4 },
+          { a: 4, b: 2, c: 3, d: 4 },
+          { a: 1, b: 2, c: 3, d: 4 }
+        ]
+      })
+    ).toEqual([[1, 2], [2, 2], [4, 2], [4, 2], [1, 2]]);
+  });
+
+  test('convert HEX to RGB', () => {
+    expect(lib.hex2rgb('#101010')).toEqual('16,16,16');
+    expect(lib.hex2rgb('101010')).toEqual('16,16,16');
+  });
+
+  test('convert RGB to HEX', () => {
+    expect(lib.rgb2hex('rgb(16,16,16)')).toEqual(['10', '10', '10']);
+    expect(lib.rgb2hex('rgb(16,16,16)')).toEqual(['10', '10', '10']);
+  });
+
+  test('uniq data', () => {
+    expect(lib.uniq([1, 2, 3, 4, 3, 3, 2, 1, 4])).toEqual([1, 2, 3, 4]);
+  });
+
+  test('chart height', () => {
+    expect(lib.getChartHeight({ type: 'sankey' }, { series: {} })).toBe(
+      '900px'
+    );
+    expect(lib.getChartHeight({ type: 'sunburst' }, { series: {} })).toBe(
+      '900px'
+    );
+    expect(lib.getChartHeight({ type: 'paralell' }, { series: {} })).toBe(
+      '900px'
+    );
+    expect(lib.getChartHeight({ type: 'bar3d' }, { series: {} })).toBe('700px');
+    expect(lib.getChartHeight({ type: 'line3d' }, { series: {} })).toBe(
+      '700px'
+    );
+    expect(lib.getChartHeight({ type: 'scatter3d' }, { series: {} })).toBe(
+      '700px'
+    );
+    expect(lib.getChartHeight({ type: 'surface' }, { series: {} })).toBe(
+      '700px'
+    );
+    expect(lib.getChartHeight({ type: 'bar' }, { series: {} })).toBe('500px');
+    expect(
+      lib.getChartHeight({ type: 'bar', height: 600 }, { series: {} })
+    ).toBe('600px');
+  });
+
+  test('坐标极值刻度确定', () => {
+    expect(lib.handleMinMax({ min: 22, max: 36 })).toEqual({
+      min: 20,
+      max: 40
+    });
   });
 });
