5b67a642da9e715056e6e9cbf33c25cc93bf8609
realeve@qq.com <realeve@qq.com>
2018-12-22 17:23:29 +0800

    update unit test

--- src/utils/users.test.js
+++ src/utils/users.test.js
@@ -2,7 +2,12 @@ import userTool from './users';
 
 test('encode and decode', () => {
   expect(userTool.getUserSetting()).toEqual({ success: false });
-  expect(userTool.clearUserSetting()).toBe(1);
+  expect(userTool.saveUserSetting()).toBeUndefined();
+  expect(userTool.clearUserSetting()).toBeUndefined();
+
+  expect(userTool.saveLastRouter()).toBeUndefined();
   expect(userTool.readLastRouter()).toBe('/menu');
+
+  expect(userTool.saveLoginStatus()).toBeUndefined();
   expect(userTool.getLoginStatus()).toBe(0);
 });
