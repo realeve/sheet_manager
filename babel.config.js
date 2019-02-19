43f1c8dfb7086662cd0baab8f4541cf8a9cc1a01
realeve@qq.com <realeve@qq.com>
2019-02-19 09:14:35 +0800

    detail fix

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  // babel 7.0
-  // presets: ['@babel/preset-env', '@babel/preset-react'],
-  // plugins: ['@babel/plugin-proposal-export-default-from'],
-  // presets: ['env', 'react'],
   presets: ['@babel/preset-react', '@babel/preset-env', '@babel/preset-typescript'], // this is equivalent
   plugins: ['@babel/plugin-transform-runtime', '@babel/transform-arrow-functions'], // same
 };
