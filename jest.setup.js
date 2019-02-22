08b656205f70e257f0a9e0463563875e4c2ecd34
realeve@qq.com <realeve@qq.com>
2019-02-22 16:17:14 +0800

    更新 jest中引用 console报错的问题

--- jest.setup.js
+++ jest.setup.js
@@ -8,15 +8,15 @@ configure({
 // For async tests, catch all errors here so we don't have to try / catch
 // everywhere for safety
 
-// process.on('unhandledRejection', error => {
-//   console.log('enzyme错误：', error);
-// });
+process.on('unhandledRejection', error => {
+  console.log('enzyme错误：', error);
+});
 
 // jest 24.x.x 版本会丢失 console
-if (!console || console.log) {
-  console.log = jest.fn(input => {
-    process.stdout.write(`${input} \n`);
-  });
-  console.warn = console.log;
-  console.error = console.log;
-}
+// if (!console || console.log) {
+//   console.log = jest.fn(input => {
+//     process.stdout.write(`${input} \n`);
+//   });
+//   console.warn = console.log;
+//   console.error = console.log;
+// }
