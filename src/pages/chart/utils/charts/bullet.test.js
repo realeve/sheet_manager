8cb8b8b6e6daa785b9192912dc18af80ea2ad9d5
realeve@qq.com <realeve@qq.com>
2019-03-05 09:35:21 +0800

    bullet 更新测试用例，调整为ts

---
+++ src/pages/chart/utils/charts/bullet.test.js
@@ -0,0 +1,200 @@
+import * as lib from './bullet';
+
+const renderAPI = { value: () => 100, coord: ([x, y]) => [x, y], visual: () => '#f22' };
+const renderDst = src => ({
+  type: 'group',
+  children: [
+    {
+      type: 'line',
+      shape: src,
+      style: {
+        stroke: '#f22',
+        lineWidth: 4,
+      },
+    },
+  ],
+});
+test('自定义渲染', () => {
+  let src = {
+    x1: 100,
+    x2: 100,
+    y1: 70,
+    y2: 120,
+  };
+  expect(lib.getShape({ x: 100, y: 100, reverse: false })).toMatchObject(src);
+  let dst = {
+    x1: 70,
+    x2: 120,
+    y1: 100,
+    y2: 100,
+  };
+  expect(lib.getShape({ x: 100, y: 100, reverse: true })).toMatchObject(dst);
+
+  expect(lib.renderItem(renderAPI, false)).toMatchObject(renderDst(src));
+});
+
+test('堆叠数据处理', () => {
+  const indexStyle = {
+    type: 'bar',
+    barWidth: 50,
+    barGap: '-130%',
+    stack: '指标范围',
+    silent: true,
+  };
+  let option1 = {
+    x: 0,
+    y: 1,
+    y2: 2,
+    level: '3,4,5,6',
+    reverse: '0',
+    max: 100,
+    data: {
+      header: ['x', 'y', 'y2', '差', '良', '中', '优'],
+      data: [
+        {
+          x: '品种1',
+          y: 70,
+          y2: 80,
+          差: 60,
+          良: 65,
+          中: 70,
+          优: 80,
+        },
+      ],
+    },
+  };
+  expect(lib.handleStackData(option1)).toMatchObject({
+    levelData: [
+      {
+        ...indexStyle,
+        name: '低于差',
+        data: [60],
+        itemStyle: { color: '#ff736e' },
+      },
+      {
+        ...indexStyle,
+        name: '差',
+        data: [5],
+        itemStyle: { color: '#FFA39E' },
+      },
+      {
+        ...indexStyle,
+        name: '良',
+        data: [5],
+        itemStyle: { color: '#FFD591' },
+      },
+      {
+        ...indexStyle,
+        name: '中',
+        data: [10],
+        itemStyle: { color: '#91D5FF' },
+      },
+      {
+        ...indexStyle,
+        name: '优',
+        data: [20],
+        itemStyle: { color: '#A7E8B4' },
+      },
+    ],
+    levelText: ['低于差', '差', '良', '中', '优'],
+  });
+
+  let option2 = {
+    x: 0,
+    y: 1,
+    y2: 2,
+    level: '3,4,5',
+    reverse: '0',
+    max: 100,
+    data: {
+      header: ['x', 'y', 'y2', '差', '良', '优'],
+      data: [
+        {
+          x: '品种1',
+          y: 70,
+          y2: 80,
+          差: 60,
+          良: 65,
+          优: 70,
+        },
+      ],
+    },
+  };
+  expect(lib.handleStackData(option2)).toMatchObject({
+    levelData: [
+      {
+        ...indexStyle,
+        data: [60],
+        itemStyle: { color: '#FFA39E' },
+        name: '低于差',
+      },
+      {
+        ...indexStyle,
+        data: [5],
+        itemStyle: { color: '#FFD591' },
+        name: '差',
+      },
+      {
+        ...indexStyle,
+        data: [5],
+        itemStyle: { color: '#91D5FF' },
+        name: '良',
+      },
+      {
+        ...indexStyle,
+        data: [30],
+        itemStyle: { color: '#A7E8B4' },
+        name: '优',
+      },
+    ],
+    levelText: ['低于差', '差', '良', '优'],
+  });
+
+  // 最终渲染结果处理
+  let dst1 = lib.bullet(option1);
+  expect(dst1.legend).toMatchObject({
+    data: [
+      { icon: 'circle', name: '目标值' },
+      { icon: 'circle', name: '实际值' },
+      { icon: 'circle', name: '低于差' },
+      { icon: 'circle', name: '差' },
+      { icon: 'circle', name: '良' },
+      { icon: 'circle', name: '中' },
+      { icon: 'circle', name: '优' },
+    ],
+    selected: [{ 低于差: false }, { 差: false }, { 良: false }, { 中: false }, { 优: false }],
+  });
+
+  expect({ xAxis: dst1.xAxis, yAxis: dst1.yAxis }).toMatchObject({
+    xAxis: { splitLine: { show: false } },
+    yAxis: {
+      axisLine: { show: false },
+      axisTick: { show: false },
+      data: ['品种1'],
+    },
+  });
+
+  expect([dst1.series[0], dst1.series[1]]).toMatchObject([
+    { barWidth: 30, data: [70], name: '目标值', type: 'bar', z: 9 },
+    { data: [80], name: '实际值', type: 'custom', z: 10 },
+  ]);
+
+  let dst2 = lib.bullet(Object.assign(option1, { reverse: true }));
+  expect({ xAxis: dst2.xAxis, yAxis: dst2.yAxis }).toMatchObject({
+    yAxis: { splitLine: { show: false } },
+    xAxis: {
+      axisLine: { show: false },
+      axisTick: { show: false },
+      data: ['品种1'],
+    },
+  });
+
+  expect(dst1.series[1].renderItem(null, renderAPI)).toMatchObject(
+    renderDst({
+      x1: 100,
+      x2: 100,
+      y1: 70,
+      y2: 120,
+    })
+  );
+});
---
+++ src/pages/chart/utils/charts/bullet.ts
@@ -0,0 +1,239 @@
+import util, { TChartConfig } from '../lib';
+
+export interface BarStyle {
+  type: string;
+  barWidth: number;
+  barGap: string;
+  stack: string;
+  silent: boolean;
+  [key: string]: any;
+}
+
+export interface IndexCfg extends BarStyle {
+  name: string;
+  data: number[];
+  itemStyle: {
+    color: string;
+  };
+}
+
+let chartConfig: TChartConfig = [
+  {
+    key: 'type',
+    title: '图表类型',
+    default: 'bullet',
+    url: [],
+  },
+  {
+    key: 'x',
+    title: 'X轴，用于需要对比的各个指标.',
+    default: 0,
+  },
+  {
+    key: 'y',
+    title: '对应指标的期望完成值',
+    default: 1,
+  },
+  {
+    key: 'y2',
+    title: '对应指标的实际完成值',
+    default: 2,
+  },
+  {
+    key: 'level',
+    title: '各指标等级，依次为差,良,中,优，用逗号隔开',
+    default:
+      '3,4,5,6 表示3-6列分别为差至优的等级。在接口中的别名分别对应展示的文字(第3列起默认为差/良/中/优)。如果评价等级只有3级，如设为3,4,5，别名设置为差、良、优',
+  },
+  {
+    key: 'max',
+    title: '指标最大值',
+    default: 100,
+    url: ['/chart#id=75/be3010cfeb&type=bullet&max=130'],
+  },
+  {
+    key: 'revert',
+    title: '翻转X/Y',
+    default: '0:false',
+    url: ['/chart#id=75/be3010cfeb&type=bullet&reverse=1', '/chart#id=75/be3010cfeb&type=bullet'],
+  },
+];
+
+const handleStackData = option => {
+  let { header, data } = option.data;
+  let levelCfg: string = option.level;
+  let levelIdx: string[] = levelCfg.split(',');
+  let levelText: string[] = levelIdx.map(idx => header[idx]);
+  levelText = ['低于' + levelText[0], ...levelText];
+  let max: number | string = option.max;
+
+  let levelColor: string[] = ['#ff736e', '#FFA39E', '#FFD591', '#91D5FF', '#A7E8B4'];
+
+  const indexStyle: BarStyle = {
+    type: 'bar',
+    barWidth: 50,
+    barGap: '-130%',
+    stack: '指标范围',
+    silent: true,
+  };
+
+  let levelDataList: any[] = levelIdx.map(level =>
+    util.getDataByIdx({
+      key: header[level],
+      data,
+    })
+  );
+  levelDataList.push(new Array(levelDataList[0].length).fill(parseInt(String(max), 10)));
+
+  // 堆叠数据还原，逆向减操作
+  for (let i: number = levelDataList.length - 1; i > 0; i--) {
+    let curCol: number[] = levelDataList[i];
+    let nextCol: number[] = levelDataList[i - 1];
+    for (let j: number = 0; j < curCol.length; j++) {
+      levelDataList[i][j] = curCol[j] - nextCol[j];
+    }
+  }
+
+  let levelData: IndexCfg[] = levelDataList.map((data, idx) => ({
+    ...indexStyle,
+    name: levelText[idx],
+    data,
+    itemStyle: {
+      color: levelColor[5 - levelText.length + idx], // 如果只有优良差三级的时候，levelText为4，区间范围自动加1
+    },
+  }));
+  return { levelData, levelText };
+};
+
+const getShape = ({ x, y, reverse }) => {
+  let shape: {
+    x1: number;
+    x2: number;
+    y1: number;
+    y2: number;
+  } = {
+    x1: x,
+    x2: x,
+    y1: y - 30,
+    y2: y + 20,
+  };
+
+  if (reverse) {
+    shape = {
+      x1: x - 30,
+      x2: x + 20,
+      y1: y,
+      y2: y,
+    };
+  }
+  return shape;
+};
+
+const renderItem = (api, reverse) => {
+  const [x, y]: number[] = api.coord([api.value(0), api.value(1)]);
+  let shape = getShape({ x, y, reverse });
+  return {
+    type: 'group',
+    children: [
+      {
+        type: 'line',
+        shape,
+        style: {
+          stroke: api.visual('color'),
+          lineWidth: 4,
+        },
+      },
+    ],
+  };
+};
+
+const bullet = option => {
+  let { header, data } = option.data;
+  option = Object.assign(
+    {
+      x: 0,
+      y: 1,
+      y2: 2,
+      level: '3,4,5,6',
+      reverse: '0',
+      max: 100,
+    },
+    option
+  );
+
+  let xIdx: number | string = option.x;
+  let targetIdx: number | string = option.y;
+  let actualIdx: number | string = option.y2;
+
+  let reverse: boolean = option.reverse == '1';
+
+  let xAxisData: any[] = util.getDataByIdx({
+    key: header[xIdx],
+    data,
+  });
+  let yAxisData: any[] = util.getDataByIdx({
+    key: header[targetIdx],
+    data,
+  });
+  let actualData: any[] = util.getDataByIdx({
+    key: header[actualIdx],
+    data,
+  });
+
+  let { levelText, levelData } = handleStackData(option);
+
+  let configX = {
+    data: xAxisData,
+    axisLine: {
+      show: false,
+    },
+    axisTick: {
+      show: false,
+    },
+  };
+  let configY = {
+    splitLine: {
+      show: false,
+    },
+  };
+
+  return {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow',
+      },
+    },
+    legend: {
+      data: ['目标值', '实际值', ...levelText].map(name => ({
+        name,
+        icon: 'circle',
+      })),
+      selected: levelText.map(key => ({
+        [key]: false,
+      })),
+    },
+    yAxis: reverse ? configY : configX,
+    xAxis: reverse ? configX : configY,
+    color: ['#47467c', '#e23273'],
+    series: [
+      {
+        type: 'bar',
+        name: '目标值',
+        data: yAxisData,
+        barWidth: 30,
+        z: 9,
+      },
+      {
+        type: 'custom',
+        name: '实际值',
+        renderItem: (_, api) => renderItem(api, reverse),
+        data: actualData,
+        z: 10,
+      },
+      ...levelData,
+    ],
+  };
+};
+
+export { bullet, chartConfig, handleStackData, getShape, renderItem };
