7dbc8e35736ec7675455f790ca5056e5e682f57a
realeve@qq.com <realeve@qq.com>
2019-03-20 19:35:16 +0800

    添加图片复制

--- src/pages/Search/components/ImageItem.tsx
+++ src/pages/Search/components/ImageItem.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import styles from '../Image.less';
 import 'animate.css';
+import copy from 'copy-to-clipboard';
+import { message } from 'antd';
 
+const prefix = 'data:image/jpg;base64,';
+// todo 增加点击复制url链接功能.
 export default function ImageItem({ data, type, ImageTitle, visible }) {
+  const copyImg = img => {
+    copy(img);
+    message.success('图像拷贝成功');
+  };
   return (
     visible &&
     data.map((item, idx) => (
-      <li key={type + idx} className="animated zoomIn">
+      <li
+        key={type + idx}
+        className="animated zoomIn"
+        onClick={() => copyImg(`${prefix}${item.image}`)}
+      >
         <div className={styles.wrap}>
-          <img src={`data:image/jpg;base64,${item.image}`} alt={item.code} />
+          <img src={`${prefix}${item.image}`} alt={item.code} />
         </div>
         <div className={styles.desc}>
           <ImageTitle data={item} />
