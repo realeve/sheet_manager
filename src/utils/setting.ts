f77b2e58b51c93652401a26b9137658cb1d0a390
realeve <realeve@qq.com>
2019-10-11 15:13:47 +0800

    :art: update config

--- src/utils/setting.ts
+++ src/utils/setting.ts
@@ -4,10 +4,10 @@ export let DEV: boolean = process.env.NODE_ENV === 'test'; //|| process.env.NODE
 export let config = {
   chengdu: {
     company: '成都印钞有限公司',
-    api: 'http://cdn.cdyc.cbpm:100',
+    api: 'http://10.8.1.25:100',
     footer: '成都印钞有限公司 印钞管理部',
     systemName: '质量信息管理平台',
-    uploadHost: 'http://cdn.cdyc.cbpm:100/upload/',
+    uploadHost: 'http://10.8.1.25:100/upload/',
     host: 'http://10.8.2.133:8000',
     org: 'CDYC',
     uap: {
